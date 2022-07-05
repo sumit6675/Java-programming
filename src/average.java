45d7f9e2ac14e06c6a6f195a6679a93d1fe6c597
sumit6675 <sumitchimkar0@gmail.com>
2022-07-06 00:50:02 +0530

    added new java file

---
+++ src/average.java
@@ -0,0 +1,17 @@
+	import java.util.Scanner ;
+public class average {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int a,b,c;
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		int avg=(a+b+c)/3;
+		System.out.println(avg);
+
+	}
+
+}
---
+++ src/helloworld.java
@@ -0,0 +1,13 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 10;
+		System.out.println(a+b);
+		System.out.println("the answer is "+ (a+b));
+
+	}
+
+}
